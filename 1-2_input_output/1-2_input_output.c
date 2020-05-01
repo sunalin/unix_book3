4a966a8df8f9dae6ba82319fb181929af5c2cf7d
sunalin <229105291@qq.com>
2020-05-01 19:58:13 +0800

    1-2_input_output

---
+++ 1-2_input_output/1-2_input_output.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+ 
+int main(int argc, char *args[])
+{
+    char buf[1024];
+
+    while (1)
+    {
+        char info[100] = "input:";
+        write(STDOUT_FILENO, info, sizeof(info));
+
+        memset(buf, 0, sizeof(buf));
+        read(STDIN_FILENO, buf, sizeof(buf));
+        
+        write(STDOUT_FILENO, buf, strlen(buf));
+    }	
+
+    return 0;
+}
+
+
+
