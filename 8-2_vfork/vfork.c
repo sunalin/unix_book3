0058cbeb15b1f94e0d114349d2483f6b96ae0ade
sunalin <229105291@qq.com>
2020-05-03 14:27:56 +0800

    8-2_vfork

---
+++ 8-2_vfork/vfork.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+
+
+int glob = 6;
+char buf[] = "a write to stdout\r\n";
+
+int main(int argc, char* args[])
+{
+    int var;
+    pid_t pid;
+
+    var = 88;
+
+    write(STDOUT_FILENO, buf, sizeof(buf) - 1);
+
+    printf("vfork befor[getpid = %d]\r\n", getpid());
+    fflush(fdopen(STDOUT_FILENO, "a+"));    // flush now
+    pid = vfork();   // create copy process
+    printf("vfork after[getpid = %d]\r\n", getpid());
+
+    if (pid < 0)
+        printf("vfork error\r\n");
+    else if (pid > 0)
+    {
+        // parent process
+        sleep(2);
+    }
+    else if (pid == 0)
+    {
+        // child process
+        glob++;
+        var++;
+    }
+
+    printf(" getpid = %d,"
+           " getppid = %d,"
+           " glob = %d,"
+           " var = %d\r\n", getpid(), getppid(), glob, var);
+
+    if (pid == 0)
+    {
+        /*
+        1.vfork()创建子进程，在调用exec()之前或exit()之前，
+          子进程与父进程共享数据段（与fork()不同，fork要拷
+          贝父进程的数据段,堆栈段）
+        2.调用vfork()后，子进程先执行，父进程被挂起，直到
+          子进程调用了exec或exit之后，父进程才执行。
+        使用vfork(),但子进程不调用exit()或exec()，父进程一直挂起，导致死锁！
+        */
+        
+        // child process
+        exit(0);    // exec/exit/_exit
+    }
+
+    //exit(0);
+}
+
+
+
