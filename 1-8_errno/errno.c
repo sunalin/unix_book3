3b5c2b4b4458af9769fbded11d914228e5e5394e
sunalin <229105291@qq.com>
2020-05-17 16:42:45 +0800

    打印错误信息 strerror() perror()

---
+++ 1-8_errno/errno.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *args[])
+{
+    fprintf(stderr, "EACCES: %s\r\n", strerror(EACCES));
+    fprintf(stderr, "ENOENT: %s\r\n", strerror(ENOENT));
+
+    errno = ENOENT;
+    perror("sunalin");
+
+    return 0;
+}
+
+
+
