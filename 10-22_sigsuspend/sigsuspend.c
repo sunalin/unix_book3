9dc475c6326aeb958adbeb1cfe95d98d0f6f890e
sunalin <229105291@qq.com>
2020-05-23 11:47:07 +0800

    sigsuspend    使进程挂起（进入睡眠状态）等待信号的中断，信号屏蔽字设为sigmask集        (sigmask为空集则sigsuspend和pause是一样的，可以接受任何信号的中断)，        如果没有信号发生，进程会一直挂起，当有信号发生时(该信号不能是sigmask屏蔽的信号)，        执行该信号处理程序，处理程序返回后sigsuspend才返回，并且信号屏蔽字恢复为sigsuspend        调用之前的状态
           需要了解：正常情况下，执行一个信号的处理程序时，该信号自动加入信号屏蔽字，处理程序
           返回时，该信号自动解除屏蔽

--- 10-22_sigsuspend/sigsuspend.c
+++ 10-22_sigsuspend/sigsuspend.c
@@ -45,33 +45,37 @@ int main(int argc, char* args[])
 {
     sigset_t new_mask;
     sigset_t old_mask;
-    sigset_t zero_mask;
+    sigset_t wait_mask;
     
     signal(SIGINT, sig_SIGINT);     // ctrl+c
 
     sigemptyset(&new_mask);
     sigaddset(&new_mask, SIGINT);
-    sigprocmask(SIG_BLOCK, &new_mask, &old_mask);   // SIGINT加入屏蔽字，不让SIGINT干扰临界区
+    sigprocmask(SIG_BLOCK, &new_mask, &old_mask);   // SIGINT  加入屏蔽字
 
     // 临界区
     pr_mask("critical [start] mask: ");
     printf("critical do something......");
     pr_mask("critical [end]   mask: ");
 
-    /* 
-       sigsuspend函数是pause函数的增强版。当sigsuspend函数的参数信号集
-       为空信号集时，sigsuspend函数是和pause函数是一样的，可以接受任何信号的中断
+    /* sigsuspend    使进程挂起（进入睡眠状态）等待信号的中断，信号屏蔽字设为sigmask集
+       (sigmask为空集则sigsuspend和pause是一样的，可以接受任何信号的中断)，
+       如果没有信号发生，进程会一直挂起，当有信号发生时(该信号不能是sigmask屏蔽的信号)，
+       执行该信号处理程序，处理程序返回后sigsuspend才返回，并且信号屏蔽字恢复为sigsuspend
+       调用之前的状态
 
-       在sigsuspend函数调用时，会使进程挂起（进入睡眠状态）等待信号的中断，
-       如果没有信号发生，进程会一直挂起，当有信号发生时，但该信号不是
-       sigsuspend函数的信号集中所设置的屏蔽的信号时，sigsuspend会处理该信号，
-       当该信号处理完成后，sigsuspend函数才返回，并执行接下来的代码
-     */
-    sigemptyset(&zero_mask);
-    //sigaddset(&zero_mask, SIGQUIT);
-    sigsuspend(&zero_mask);
+       需要了解：正常情况下，执行一个信号的处理程序时，该信号自动加入信号屏蔽字，处理程序
+       返回时，该信号自动解除屏蔽 */
+    //int sigsuspend(const sigset_t* sigmask);
+
+    sigemptyset(&wait_mask);
+    sigaddset(&wait_mask, SIGUSR1);     // SIGUSR1  加入屏蔽字
+    printf("sigsuspend [SIGUSR1]\r\n");
+    sigsuspend(&wait_mask);
     pr_mask("sigsuspend after mask: ");
 
+
+    //
     sigprocmask(SIG_SETMASK, &old_mask, 0);
 
     exit(0);
