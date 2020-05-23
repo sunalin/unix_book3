0e3fe3afa0cc75ae8e7790cb0bbd3d37e3482a44
sunalin <229105291@qq.com>
2020-05-23 21:43:31 +0800

    pthread_join    阻塞等待线程终止并回收资源，更改ret指向=pthread_exit回传的指针参数        pthread_cleanup_push与pthread_cleanup_pop需要成对存在，用于注册线程清理        程序(return或pthread_exit调用)，pthread_cleanup_pop的参数非零时才有效

--- 11-4_pthread_join/pthread_join.c
+++ 11-4_pthread_join/pthread_join.c
@@ -14,11 +14,23 @@
 #include <time.h>
 #include <pthread.h>
 
+
+void cleanup(void* arg)
+{
+    printf("[%s] cleanup\r\n", arg ? ((char*)arg) : "");
+}
+
 void* thread_1(void* arg)
 {
     static int ret_val = 0;
     ret_val = 5;
     printf("thread_1 run...  ret_val[%d] &ret_val[0x%lx]\r\n", ret_val, (long)&ret_val);
+
+    pthread_cleanup_push(cleanup, "thread_1 c1");
+    pthread_cleanup_pop(1);
+    pthread_cleanup_push(cleanup, "thread_1 c2");
+    pthread_cleanup_pop(0);
+
     return (void*)&ret_val;
 }
 
@@ -27,6 +39,12 @@ void* thread_2(void* arg)
     static int ret_val = 0;
     ret_val = 6;
     printf("thread_2 run...  ret_val[%d] &ret_val[0x%lx]\r\n", ret_val, (long)&ret_val);
+
+    pthread_cleanup_push(cleanup, "thread_2 c1");
+    pthread_cleanup_pop(0);
+    pthread_cleanup_push(cleanup, "thread_2 c2");
+    pthread_cleanup_pop(1);
+
     pthread_exit(&ret_val);
 }
 
@@ -36,14 +54,16 @@ int main(int argc, char* args[])
     pthread_t tid2;
     int* ret;
 
-    pthread_create(&tid1, 0, thread_1, 0);
-    pthread_create(&tid2, 0, thread_2, 0);
+    pthread_create(&tid1, 0, thread_1, (void*)0);
+    pthread_create(&tid2, 0, thread_2, (void*)0);
 
-    /* pthread_join    阻塞等待线程终止，更改ret指向=pthread_exit回传的指针参数 */
+    /* pthread_join    阻塞等待线程终止并回收资源，更改ret指向=pthread_exit回传的指针参数
+       pthread_cleanup_push与pthread_cleanup_pop需要成对存在，用于注册线程清理
+       程序(return或pthread_exit调用)，pthread_cleanup_pop的参数非零时才有效 */
     pthread_join(tid1, (void*)&ret);
     printf("thread_1 return  *ret[%d] ret[0x%lx]\r\n", *ret, (long)ret);
     pthread_join(tid2, (void*)&ret);
-    printf("thread_1 return  *ret[%d] ret[0x%lx]\r\n", *ret, (long)ret);
+    printf("thread_2 return  *ret[%d] ret[0x%lx]\r\n", *ret, (long)ret);
 
            
     exit(0);
