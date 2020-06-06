5fd62a05209db0db023684273f205541eed13663
sunalin <229105291@qq.com>
2020-06-06 23:35:22 +0800

    unlink 将path文件的链接计数-1，当计数为0时由内核删除文件内容，        如果有进程打开了文件，内容不能删除，        关闭文件时，内核检查该文件打开的进程个数，为0时才检查链接计数

--- 04-16_unlink/unlink.c
+++ 04-16_unlink/unlink.c
@@ -12,11 +12,10 @@ int main(int argc, char* args[])
 {
     int fd = open("./foo1", O_WRONLY|O_CREAT|O_TRUNC);
 
-    /* unlink
-       将path文件的链接计数-1，当计数为0时由内核删除文件内容，
+    /* unlink 将path文件的链接计数-1，当计数为0时由内核删除文件内容，
        如果有进程打开了文件，内容不能删除，
-       关闭文件时，内核检查该文件打开的进程个数，为0时才检查链接计数
-     */
+       关闭文件时，内核检查该文件打开的进程个数，为0时才检查链接计数 */
+
     unlink("./foo1");   // foo1被打开了
 
     close(fd);
